680a1297dce6fe42912aac0f64a7c9c0542594b7
Anjana Rajan <anjana.rajan@wavetronix.com>
2026-09-10 12:55:25 +0530

    adding feature:square to the latest dev

--- calculator.cpp
+++ calculator.cpp
@@ -52,7 +52,7 @@ int main()
 			case 5:
 			cout<<"give the number";
             cin>>firstNumber;
-            int result = sqrtl(firstNumber);
+            result = sqrtl(firstNumber);
 			cout<<"the square root of"<<firstNumber<<"is"<<result;
             break;
 			
@@ -71,6 +71,7 @@ int main()
 			  result = firstNumber+secondNumber;
 			  cout << "Result of Addition = "<<result;
 			  break;
+			  
 			case 3:
 				cout << "Enter first number: ";
 				cin >> firstNumber;
